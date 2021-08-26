e9307823882eb2e6cd747ef01ffc949f30059699
Stan Boer <stanboer@gmail.com>
2021-08-26 16:21:07 +0200

    Initiated C# project

---
+++ FTLCockpit/Controller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace HelloWorld
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    public class Hello : MonoBehaviour
+    {
+        public void Update()
+        {
+            Debug.Log("Hello world! " + Time.realtimeSinceStartup);
+        }
+    }
+}
