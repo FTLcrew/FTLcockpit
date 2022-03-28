206b41631e73f7db08138e46dc738baa5cd7a0f8
Dragonstei <stanboer@gmail.com>
2022-03-28 18:14:28 +0200

    Added SAS toggle

--- FTLcockpit/ftlcockpit.ino
+++ FTLcockpit/ftlcockpit.ino
@@ -1,17 +1,15 @@
-/* KerbalSimpit
-   A very barebones Hello World sketch, that doesn't require
-   any extra hardware. It periodically sends EchoRequest packets
-   to the game, and uses the EchoReply packets to switch the
-   on-board LED on and off. Note that the game only activates the
-   echo handler during the flight scene.
-   Great for confirming basic connectivity.
-
-   Peter Hardy <peter@hardy.dropbear.id.au>
+/* FTL Cockpit
+   Software to control Kerbal Space Program with the FTL Cockpit
 */
+
 #include "KerbalSimpit.h"
 
-// Declare a KerbalSimpit object that will
-// communicate using the "Serial" device.
+// Input setup
+const int SAS_SWITCH_PIN = 3; // the pin used for controlling SAS.
+
+byte currentActionStatus = 0;
+
+// Declare a KerbalSimpit object that will communicate using the "Serial" device.
 KerbalSimpit ftlCockpit(Serial);
 
 // This boolean tracks the desired LED state.
@@ -22,6 +20,10 @@ unsigned long lastSent = 0;
 // How often to send echo packets (in ms)
 unsigned int sendInterval = 1000;
 
+// SAS button state
+int sasLastState = 0;
+
+
 void setup() {
   // Open the serial connection.
   Serial.begin(115200);
@@ -29,6 +31,10 @@ void setup() {
   // Set up the built in LED, and turn it on.
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
+
+  // Set up the switch with builtin pullup.
+  pinMode(SAS_SWITCH_PIN, INPUT_PULLUP);
+  
   // This loop continually attempts to handshake with the plugin.
   // It will keep retrying until it gets a successful handshake.
   while (!ftlCockpit.init()) {
@@ -38,13 +44,20 @@ void setup() {
   digitalWrite(LED_BUILTIN, LOW);
   // Display a message in KSP to indicate handshaking is complete.
   ftlCockpit.printToKSP("Connected", PRINT_TO_SCREEN);
+  
   // Sets our callback function. The KerbalSimpit library will
   // call this function every time a packet is received.
   ftlCockpit.inboundHandler(messageHandler);
+  
+  // Send a message to the plugin registering for the Action status channel.
+  // The plugin will now regularly send Action status  messages while the
+  // flight scene is active in-game.
+  ftlCockpit.registerChannel(ACTIONSTATUS_MESSAGE);
 }
 
 void loop() {
   unsigned long now = millis();
+  
   // If we've waited long enough since the last message, send a new one.
   if (now - lastSent >= sendInterval) {
     // If the last message was "high", send "low"
@@ -59,13 +72,32 @@ void loop() {
     // Update the state variable to match the message we just sent.
     state = !state;
   }
-  // Call the library update() function to check for new messages.
+  
+  // Get the SAS switch state
+  int sas_switch_state = digitalRead(SAS_SWITCH_PIN);
+
+  // Update the SAS to match the state, only if a change is needed to avoid
+  // spamming commands.
+  if(sas_switch_state != sasLastState){
+    if(sas_switch_state && !(currentActionStatus & SAS_ACTION)){
+      ftlCockpit.printToKSP("Activate SAS!");
+      ftlCockpit.activateAction(SAS_ACTION);
+    }
+    else if(!sas_switch_state && (currentActionStatus & SAS_ACTION)){
+      ftlCockpit.printToKSP("Desactivate SAS!");
+      ftlCockpit.deactivateAction(SAS_ACTION);
+    }
+    sasLastState = !sasLastState;
+  }
+
+  // Delay for debouncing -> change to milis
+  delay(20);
   ftlCockpit.update();
 }
 
 void messageHandler(byte messageType, byte msg[], byte msgSize) {
-  // We are only interested in echo response packets.
-  if (messageType == ECHO_RESP_MESSAGE) {
+  switch(messageType) {
+  /*case ECHO_RESP_MESSAGE:
     // The message payload will be either "low" or "high".
     // We use the strcmp function to check what the string payload
     // is, and set the LED status accordingly.
@@ -74,5 +106,18 @@ void messageHandler(byte messageType, byte msg[], byte msgSize) {
     } else {
       digitalWrite(LED_BUILTIN, HIGH);
     }
+  */
+  case ACTIONSTATUS_MESSAGE:
+    if (msgSize == 1) {
+      currentActionStatus = msg[0];
+
+      //Let the LED_BUILIN match the current SAS state
+      if(currentActionStatus & SAS_ACTION){
+        digitalWrite(LED_BUILTIN, HIGH);
+      } else {
+        digitalWrite(LED_BUILTIN, LOW);
+      }
+    }
+    break;
   }
 }
